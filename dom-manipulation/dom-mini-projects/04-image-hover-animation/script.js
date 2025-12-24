dc90b8d0d90dea803708202f4f149ea946600330
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2025-12-24 21:34:44 +0530

    Add cursor-follow image hover animation

--- dom-manipulation/dom-mini-projects/04-image-hover-animation/script.js
+++ dom-manipulation/dom-mini-projects/04-image-hover-animation/script.js
@@ -1,2 +1,13 @@
-// JavaScript logic will be added in the next commit
-// This file is intentionally kept for project structure
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function (val) {
+  val.addEventListener("mouseenter", function () {
+    val.childNodes[3].style.opacity = 1
+  });
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0
+  });
+  val.addEventListener("mousemove", function (dets) {
+    val.childNodes[3].style.left = dets.x+"px"
+  });
+});
