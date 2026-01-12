b2acbc5699924b8a2ec54aaaaa7bcafddd77b538
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-01-12 14:49:50 +0530

    feat: add rock-paper-scissors game logic

---
+++ control-flow/rock-paper-scissor-game/script.js
@@ -0,0 +1,13 @@
+//rock-paper-scossor-game
+
+function rps(user, computer){
+    if(user === computer) return "draw";
+
+    if (user === "rock" && computer === "scissor") return "user";
+    if (user === "scissor" && computer === "paper") return "user";
+    if (user === "paper" && computer === "rock") return "user";
+
+    return "computer";
+}
+
+console.log(rps("scissor", "rock")) ;
