85480da63c2c65cb96423bd6fcd1853248146ace
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-01-14 14:51:52 +0530

    Print numbers from 1 and stop at first multiple of 7 using break

---
+++ control-flow/loops/q10-stop-at-first-multiple-of-7.js/script.js
@@ -0,0 +1,11 @@
+// Stop at first multiple of 7
+// Write a loop from 1 to 100 that :
+// Prints each number
+// Stops completely when it finds the first number divisible by 7
+
+for(i=1; i<=100; i++){
+    if(i%7===0){
+        break;
+    }
+    console.log(i);
+}
