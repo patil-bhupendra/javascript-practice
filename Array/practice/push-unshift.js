287afd4950e2dd9477c04052535319681255eac7
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-02-18 13:28:58 +0530

    Add Array example to insert elements using push() and unshift()

---
+++ Array/practice/push-unshift.js
@@ -0,0 +1,6 @@
+//Add "Mango" at the end and "pineapple at the beginning id this array:"
+
+let fruit = ["apple", "guava", "banana"]
+fruit.push("Mango")
+fruit.unshift("Pineapple")
+console.log(fruit)
