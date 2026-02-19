98c84f4bf0efd2222ceccd3dfc763eb6fd714470
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-02-19 19:28:42 +0530

    Demonstrate extracting values using array destructuring

---
+++ Array/practice/destructure.js
@@ -0,0 +1,6 @@
+// Destructure this array to get firstName and lastName
+
+let fullName = ["Bhupendra", "Patil"];
+let [firstName, lastName] = fullName;
+console.log(firstName);
+console.log(lastName);
