d23ede48bd9812cdbef54d5c66fa65a11e5bd9b9
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-12 01:38:27 +0530

    Add ES6 class example with constructor, write() and erase() methods for DOM text manipulation

---
+++ oops/es6-classes/script.js
@@ -0,0 +1,23 @@
+class CreatePencil {
+  constructor(name, company, price, color) {
+    this.name = name;
+    this.company = company;
+    this.price = price;
+    this.color = color;
+  }
+  erase() {
+    document.body.querySelectorAll("h1").forEach((elem) => {
+      if (elem.style.color == this.color) {
+        elem.remove();
+      }
+    });
+  }
+  write(text) {
+    let h1 = document.createElement("h1");
+    h1.textContent = text;
+    h1.style.color = this.color;
+    document.body.appendChild(h1);
+  }
+}
+let p1 = new CreatePencil("nataraj", "nataraj", 10, "black");
+let p2 = new CreatePencil("apsara", "apsara", 15, "blue");
