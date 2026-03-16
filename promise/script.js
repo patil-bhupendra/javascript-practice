54a72fb6d782e9fc50c89b69ad735eb4ba361a0a
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-16 09:20:23 +0530

    Add JavaScript Promise example with then and catch

--- promise/script.js
+++ promise/script.js
@@ -1,8 +1,15 @@
 let pr = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Bhupendra");
-  }, 5000);
+    let rn = Math.floor(Math.random() * 10);
+    if (rn > 5) resolve("resolved with " + rn);
+    else reject("rejected with " + rn);
+  }, 3000);
 });
+
 pr.then((result) => {
   console.log(result);
+});
+
+pr.catch((error) => {
+  console.log(error);
 });
