28e5f6758aa2b6fad95d48f83d7a2ac6317f4a81
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-03-18 16:51:17 +0530

    Implement debounce for input event handling

---
+++ debouncing/script.js
@@ -0,0 +1,18 @@
+let input = document.querySelector("input");
+
+function debounce(fnc, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fnc(...args);
+    }, delay);
+  };
+}
+
+input.addEventListener(
+  "input",
+  debounce(function () {
+    console.log("hey");
+  }, 1000),
+);
